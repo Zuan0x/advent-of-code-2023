d41c9caf0caf765bde4e451151ccee14511963f5
Zuan0x <zuan.0x@gmail.com>
2023-12-04 09:04:34 +0000

    Day 4 task 2

--- 4/day4.go
+++ 4/day4.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,7 +19,8 @@ func main() {
 	// Split the file into lines
 	cards := strings.Split(string(file), "\n")
 
-	task1(cards)
+	//ask1(cards)
+	task2(cards)
 }
 
 func task1(cards []string) {
@@ -71,4 +73,73 @@ func task1(cards []string) {
 	}
 
 	fmt.Println(total)
+}
+
+func task2(cards []string) {
+	//Create a map to keep track of the number of cards we have
+	cardMap := make(map[string]int)
+
+	for i := 0; i < len(cards); i++ {
+		//Split each card into winning and your numbers
+		allCards := strings.Split(cards[i], ": ")
+		cardIndex := strings.TrimSpace(strings.Split(string(allCards[0]), "Card")[1])
+		cardMap[cardIndex] += 1
+		if cardMap[cardIndex] != 0 {
+
+			cardNumbers := strings.Split(string(allCards[1]), " | ")
+			winningNumbers := strings.Split(string(cardNumbers[0]), " ")
+			yourNumbers := strings.Split(string(cardNumbers[1]), " ")
+
+			//Create a map to keep track of the numbers
+			numbers := make(map[string]int)
+
+			//Loop through each number in the winning numbers
+			for j := 0; j < len(winningNumbers); j++ {
+				if string(winningNumbers[j]) != "" {
+					//Add the number to the map
+					numbers[strings.TrimSpace((winningNumbers[j]))] = 0
+				}
+			}
+
+			//Loop through each number in your numbers
+			for j := 0; j < len(yourNumbers); j++ {
+				//If the number is in the map, increment the count
+				if _, ok := numbers[string(yourNumbers[j])]; ok {
+					numbers[strings.TrimSpace((yourNumbers[j]))]++
+				}
+			}
+
+			count := 0
+			//Loop through each number in the map
+			for _, value := range numbers {
+				if value != 0 {
+					//Add the number to the total
+					count++
+				}
+			}
+
+			//Add the number of cards to the map
+			for i := 0; i < count; i++ {
+				cardIndexInt, err := strconv.Atoi(cardIndex)
+				if err != nil {
+					// ... handle error
+					panic(err)
+				}
+
+				newIndex := cardIndexInt + i + 1
+				cardMap[strconv.Itoa(newIndex)] += cardMap[cardIndex]
+			}
+		}
+
+	}
+
+	total := 0
+
+	for _, value := range cardMap {
+		//Add the number to the total
+		total += value
+	}
+
+	fmt.Println(total)
+
 }
