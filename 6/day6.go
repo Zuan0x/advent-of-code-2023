9e4748d0d54c68b5c3ebd32f327215c5f5ae9795
Zuan0x <zuan.0x@gmail.com>
2023-12-06 10:05:46 +0000

    Day 6 task 2

--- 6/day6.go
+++ 6/day6.go
@@ -18,7 +18,8 @@ func main() {
 	// Split the file into lines
 	rows := strings.Split(string(file), "\n")
 
-	task1(rows)
+	//task1(rows)
+	task2(rows)
 }
 
 func task1(rows []string) {
@@ -49,6 +50,68 @@ func task1(rows []string) {
 	}
 
 	fmt.Println(total)
+}
+
+func task2(rows []string) {
+	times := strings.Split(strings.Split(rows[0], ":")[1], " ")
+	//Trim spaces
+	for i := 0; i < len(times); i++ {
+		times[i] = strings.Trim(times[i], " ")
+	}
+	joinedTime := strings.Join(times, "")
+	joinedTimeInt := convertStrToInt(joinedTime)
+	distances := strings.Split(strings.Split(rows[1], ":")[1], " ")
+	//Trim spaces
+	for i := 0; i < len(distances); i++ {
+		distances[i] = strings.Trim(distances[i], " ")
+	}
+	joinedDistance := strings.Join(distances, "")
+	joinedDistanceInt := convertStrToInt(joinedDistance)
+
+	total := 1
+
+	start := 0
+	//Loop through button hold times until you find a winning time
+	for j := 0; j < joinedTimeInt; j++ {
+		//Loop through each distance
+
+		fmt.Println(j, j*(joinedTimeInt-j))
+		distanceTravelled := j * (joinedTimeInt - j)
+
+		if distanceTravelled > joinedDistanceInt {
+			fmt.Println("WIN")
+			start = j - 1
+			break
+		}
+	}
+
+	end := 0
+	//Loop through button hold times until you find a winning time
+	for j := joinedTimeInt; j > 0; j-- {
+		//Loop through each distance
+
+		fmt.Println(j, j*(joinedTimeInt-j))
+		distanceTravelled := j * (joinedTimeInt - j)
+
+		if distanceTravelled > joinedDistanceInt {
+			fmt.Println("WIN END")
+			end = j
+			break
+		}
+	}
+
+	total = end - start
+
+	fmt.Println(start, end, total)
+}
+
+func convertStrToInt(str string) int {
+	output, err := strconv.Atoi(strings.Trim(str, " "))
+	if err != nil {
+		// ... handle error
+		panic(err)
+	}
+	return output
 }
 
 func convertStringArrayToIntArray(stringArray []string) ([]int, error) {
