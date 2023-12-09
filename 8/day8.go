5742b7fee2fb1349635b5357c688fa62b9fbbb58
Zuan0x <zuan.0x@gmail.com>
2023-12-09 08:55:40 +0000

    Day 8 task 2

--- 8/day8.go
+++ 8/day8.go
@@ -14,8 +14,8 @@ func main() {
 		return
 	}
 
-	task1(string(file))
-	//task2(rows)
+	//task1(string(file))
+	task2(string(file))
 }
 
 type tNode struct {
@@ -73,5 +73,91 @@ func task1(file string) {
 	}
 
 	fmt.Println(idx)
+}
 
+func task2(file string) {
+	rows := strings.Split(file, "\n\n")
+
+	instructions := rows[0]
+	nodeList := strings.Split(rows[1], "\n")
+
+	fmt.Println(instructions)
+	fmt.Println(nodeList)
+
+	nodes := make(map[string]tNode)
+
+	for _, node := range nodeList {
+
+		nodeParts := strings.Split(node, " = ")
+		nodeKey := nodeParts[0]
+
+		trimmedString := strings.Trim(nodeParts[1], "()")
+
+		// Split the string by comma and trim spaces
+		destinations := strings.Split(trimmedString, ",")
+
+		fmt.Println(node, nodeKey, destinations)
+		nodes[nodeKey] = tNode{
+			left:  strings.Trim(destinations[0], " "),
+			right: strings.Trim(destinations[1], " "),
+		}
+	}
+
+	fmt.Println(LCM(100, 200))
+
+	ret := 1
+
+	for start, _ := range nodes {
+		if string(start[len(start)-1]) == "A" {
+			fmt.Println(start)
+			ret = LCM(ret, solveSteps(start, instructions, nodes))
+		}
+	}
+
+	fmt.Println(ret)
+}
+
+func solveSteps(start string, instructions string, nodes map[string]tNode) int {
+	pos := start
+	idx := 0
+	for {
+		direction := string(instructions[idx%len(instructions)])
+
+		if direction == "R" {
+			pos = nodes[pos].right
+		} else if direction == "L" {
+			pos = nodes[pos].left
+		} else {
+			fmt.Println("Error")
+		}
+
+		idx++
+
+		if string(pos[len(pos)-1]) == "Z" {
+			break
+		}
+	}
+	fmt.Println(start, idx)
+	return idx
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
